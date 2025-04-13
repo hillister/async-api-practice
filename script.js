bfe2be2bfe12fccdb1e32ad3e9b301f07fb2f1ff
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2025-04-13 16:41:37 +0100

    fetch user data and user posts from api and stored it

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('hi')
+async function data(){
+    const getUser = await fetch('https://jsonplaceholder.typicode.com/users/1')
+    const userData = await getUser.json()
+    const getPosts = await fetch('https://jsonplaceholder.typicode.com/posts?userId=1')
+    const userPosts = await getPosts.json()
+    console.log(userData, userPosts)
+}
+
+data()
