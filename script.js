925f2bc168ba733d41af5f87ece1bd99e7d308ce
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2025-04-13 16:46:29 +0100

    add try and catch

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 async function data(){
-    const getUser = await fetch('https://jsonplaceholder.typicode.com/users/1')
-    const userData = await getUser.json()
-    const getPosts = await fetch('https://jsonplaceholder.typicode.com/posts?userId=1')
-    const userPosts = await getPosts.json()
-    console.log(userData, userPosts)
+    try {
+        const getUser = await fetch('https://jsonplaceholder.typicode.com/users/1')
+        const userData = await getUser.json()
+        const getPosts = await fetch('https://jsonplaceholder.typicode.com/posts?userId=1')
+        const userPosts = await getPosts.json()
+    } catch(err){
+        console.error(err);
+    }  
 }
 
 data()
