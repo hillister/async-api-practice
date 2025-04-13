44f8eb12bd690a9ce3f928085c26b91b2e53f1d9
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2025-04-13 17:03:31 +0100

    display info

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 async function data(){
     try {
         const getUser = await fetch('https://jsonplaceholder.typicode.com/users/1')
-        const userData = await getUser.json()
-        console.log('User:', userData.name)
+        const userData = await getUser.json();
+
+        const h1 = document.querySelector('h1');
+        h1.innerHTML = 'User: ' + userData.name;
         
         const getPosts = await fetch('https://jsonplaceholder.typicode.com/posts?userId=1')
-        const userPosts = await getPosts.json()
+        const userPosts = await getPosts.json();
 
-        console.log('Posts:')
+        let p = document.querySelector('p')
         userPosts.forEach(userPost => {
-            console.log(userPost.title);
+            p.innerHTML += userPost.title + '<br>';
         })
 
     } catch(err){
@@ -17,4 +19,4 @@ async function data(){
     }  
 }
 
-
+data()
