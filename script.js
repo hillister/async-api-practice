cc0681a715051325556c3936c2f5ec078c7c5199
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2025-04-13 16:51:08 +0100

    displayed user posts and name

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ async function data(){
     try {
         const getUser = await fetch('https://jsonplaceholder.typicode.com/users/1')
         const userData = await getUser.json()
+        console.log('User:', userData.name)
+        
         const getPosts = await fetch('https://jsonplaceholder.typicode.com/posts?userId=1')
         const userPosts = await getPosts.json()
+
+        console.log('Posts:')
+        userPosts.forEach(userPost => {
+            console.log(userPost.title);
+        })
+
     } catch(err){
         console.error(err);
     }  
